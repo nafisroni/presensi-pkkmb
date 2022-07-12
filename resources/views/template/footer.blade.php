d9b0b1dbc86e25bec371048d7cb391b3d1259eda
nafisroni <bawangm74@gmail.com>
2022-07-12 20:11:14 +0700

    tambah tamplate admin

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        PKKMB FMIPA UNNES
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 <a href="https://adminlte.io">presensi pkkmb</a>.</strong> All rights reserved.
+</footer>
