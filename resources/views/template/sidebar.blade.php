7511f0022a5779c4e13c7c242d8d21e274b1022e
nafisroni <bawangm74@gmail.com>
2022-07-12 21:03:09 +0700

    Tambah Middleware

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -33,7 +33,8 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+                with font-awesome or any other icon font library -->
+                @if (auth()->user()->level=="mahasiswa")
                 <li class="nav-item">
                     <a href="#" class="nav-link ">
                         <i class="nav-icon fas fa-clock"></i>
@@ -57,7 +58,8 @@
                         </li>
                     </ul>
                 </li>
-                <li class="nav-item">
+                @endif
+                <li class="nav-item has-treeview">
                     <a href="#" class="nav-link ">
                         <i class="nav-icon fas fa-file"></i>
                         <p>
@@ -66,19 +68,31 @@
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
+                        @if (auth()->user()->level=="mahasiswa")
                         <li class="nav-item">
                             <a href="#" class="nav-link active">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Presensi Mahasiswa</p>
                             </a>
                         </li>
+                        @endif
+                        @if (auth()->user()->level=="admin")
                         <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Presensi Keseluruhan</p>
                             </a>
                         </li>
+                        @endif
                     </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('logout')}}" class="nav-link ">
+                        <i class="nav-icon fas fa-arrow-right"></i>
+                        <p>
+                            LOGOUT
+                        </p>
+                    </a>
                 </li>
             </ul>
         </nav>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [HomeController::class, 'index']);
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/postLogin', [LoginController::class, 'postLogin'])->name('postLogin');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::group(['middleware' => ['auth', 'ceklevel:admin,mahasiswa']], function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
